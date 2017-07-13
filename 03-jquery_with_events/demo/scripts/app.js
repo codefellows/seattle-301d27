a39c203d04601c1e6cfe952d167ac44337d362fc
stephanielingwood <stephanielingwood@gmail.com>
2017-07-12 18:14:51 -0700

    add class 3 demo code

---
+++ 03-jquery_with_events/demo/scripts/app.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var state = {
+  correctTree: {},
+  wrongTree: {}
+};
+
+function setup () {
+  // call this with a random name
+  state.correctTree = generateRandomTree();
+  state.wrongTree = generateRandomTree();
+  renderTreeName(state.correctTree);
+  renderTreeImage(state.correctTree, 'left');
+  renderTreeImage(state.wrongTree, 'right');
+}
+
+setup();
+
+
+
+$('#treeImages').on('click', 'img', function (event) {
+  console.log($(this))
+  var answer = $(this).data('name');
+console.log(answer)
+  if (answer === state.correctTree.displayName) {
+    renderResponse('woohoo!');
+  } else {
+    renderResponse('wrong! virus alert.');
+  }
+  setup();
+});
+
+function generateRandomTree () {
+  var index = Math.floor(Math.random() * trees.length);
+  return trees[index];
+}
+
+function renderTreeName (tree) {
+  $('#treeName').text(tree.displayName);
+}
+
+function renderTreeImage (tree, position) {
+  var $img;
+
+  if (position === 'left') {
+    $img = $('#treeImages :first-child');
+  } else {
+    $img = $('#treeImages :nth-child(2)');
+  }
+  $img.attr('src', 'images/' + tree.fileName + '.jpeg');
+  $img.data('name', tree.displayName);
+  // data ^^ is the same as doing it this way via the attribute:
+  // $img.attr('data-name', tree.displayName);
+}
+
+function renderResponse (response) {
+  $('#response p').text(response);
+}
+
+// make a dropdown
+function populateCheatDropdown() {
+  var dropdownOptions = [];
+
+  trees.forEach(function (tree) {
+    // do stuff - tree.displayName
+    var $newOption = $('#cheat option').clone();
+
+    $newOption.val(tree.fileName);
+    $newOption.text(tree.displayName);
+    dropdownOptions.push($newOption);
+  });
+
+  dropdownOptions.forEach(function (option) {
+    $('select').append(option);
+  });
+  // iterate over trees array
+  // for each object get the display name
+  // add an option
+    // create an option element
+    // append will be involved
+    // append it to the dropdown, which is select
+}
+
+populateCheatDropdown();
+
+// make dropdown handler
+
+$('select').on('change', function (event) {
+  event.preventDefault();
+  // get the value of the option selected
+  // show the image somehow
+    // make the src attribute be...the fileName
+    // iterate thru the array to find
+  if (!$(this).val()) {
+    $('#cheat img').removeAttr('src');
+    return;
+  }
+
+  var filePath = 'images/' + $(this).val() + '.jpeg';
+  // find the image and change the src attribute
+  $('#cheat img').attr('src', filePath);
+});
