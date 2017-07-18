b039e00a24f024acbcb421164f803b0fc84bc989
stephanielingwood <stephanielingwood@gmail.com>
2017-07-17 17:37:52 -0700

    Add class 6 demo

---
+++ 06-ajax_and_JSON/demo/scripts/trees.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var trees = [
+  {
+    displayName: 'Big Leaf Maple',
+    fileName: 'Big_Leaf_Maple'
+  },
+  {
+    displayName: 'Douglas-fir',
+    fileName: 'Douglas-fir'
+  },
+  {
+    displayName: 'Madrona',
+    fileName: 'Madrona'
+  },
+  {
+    displayName: 'Pacific Silver Fir',
+    fileName: 'Pacific_Silver_Fir'
+  },
+  {
+    displayName: 'Ponderosa Pine',
+    fileName: 'Ponderosa_Pine'
+  },
+  {
+    displayName: 'Sitka Spruce',
+    fileName: 'Sitka_Spruce'
+  },
+  {
+    displayName: 'Western Hemlock',
+    fileName: 'Western_Hemlock'
+  },
+  {
+    displayName: 'Western Red Cedar',
+    fileName: 'Western_Red_Cedar'
+  }
+];
